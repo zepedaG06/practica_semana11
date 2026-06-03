4b25cf92f5c1fed7c8a3fd0dac7e7ee2f19a0d7f
zepedaG06 <jazepeda@uamv.edu.ni>
2026-06-02 22:20:09 -0600

    cambios en detalle, incidencia y documentocomercial

--- facturacion_prac/src/main/java/ni/edu/uam/facturacion_prac/model/Detalle.java
+++ facturacion_prac/src/main/java/ni/edu/uam/facturacion_prac/model/Detalle.java
@@ -1,6 +1,10 @@
 package ni.edu.uam.facturacion_prac.model;
 import javax.persistence.*;
 import lombok.*;
+import org.openxava.annotations.Depends;
+import org.openxava.annotations.Stereotype;
+
+import java.math.BigDecimal;
 
 
 @Embeddable @Getter @Setter
@@ -12,4 +16,11 @@ public class Detalle
     @ManyToOne(fetch = FetchType.LAZY, optional = true)
     Producto producto;
 
+    @Stereotype("DINERO")
+    @Depends("producto.numero, cantidad")
+    public BigDecimal getImporte()
+    {
+        if(producto == null || producto.getPrecio() == null) return BigDecimal.ZERO;
+        return new BigDecimal(cantidad).multiply(producto.getPrecio());
+    }
 }
--- facturacion_prac/src/main/java/ni/edu/uam/facturacion_prac/model/Incidencia.java
+++ facturacion_prac/src/main/java/ni/edu/uam/facturacion_prac/model/Incidencia.java
@@ -1,17 +1,17 @@
 package ni.edu.uam.facturacion_prac.model;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+import lombok.*;
+
 public class Incidencia
 {
-    int numero;
-    String descripcion;
-
-    public String getDescripcion() {
-        if (descripcion == null) return "Todavía sin descripción";
-        return descripcion;
+    @Getter @Setter
+    int cantidad;
+    @Getter @Setter
+    int precio;
+    public int getImporte() {
+        return cantidad * precio;
     }
 }
+
+
